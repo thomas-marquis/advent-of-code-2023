05b2f7f5870be75e1b9b05ad095da2d42640bbd0
Thomas Marquis <thomas.marquis.dev@gmail.com>
2023-12-05 08:16:26 +0100

    day3 part 1

--- days/day3.go
+++ days/day3.go
@@ -1,10 +1,36 @@
 package days
 
 import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/thomas-marquis/advent-of-code-2023/utils"
 )
+
+type engineNumber struct {
+	Value string
+	lineIndex int
+	startIndex int
+	endIndex int
+	aroundSymbols []string
+}
+
+func (n *engineNumber) IsEnginePart() bool {
+	symbolsRegexp := regexp.MustCompile(`[^a-zA-Z\d.]`)
+
+	var isEnginePart bool
+	for _, symbol := range n.aroundSymbols {
+		if symbolsRegexp.MatchString(symbol) {
+			isEnginePart = true
+			break
+		}
+	}
+
+	return isEnginePart
+}
 
 
 type matrix struct {
@@ -22,21 +48,81 @@ func (m *matrix) NbColumns() int {
 	return len(m.Content[0])
 }
 
-func (m *matrix) NextNumber() (string, error) {
-	if m.currentLine >= len(m.NbLines()) {
-		return "", errors.New("End of matrix")
+func (m *matrix) MoveRight() {
+	m.currentColumn++
+}
+
+func (m *matrix) MoveDown() {
+	m.currentLine++
+}
+
+func (m *matrix) NextLine() {
+	m.MoveDown()
+	m.currentColumn = 0
+}
+
+func (m *matrix) FillAroundSymbols(n *engineNumber) {
+	if n.startIndex > 0 {
+		n.aroundSymbols = append(n.aroundSymbols, m.Content[n.lineIndex][n.startIndex-1])
 	}
-	if m.currentColumn >= len(m.Content[m.currentLine]) {
-		m.currentLine++
-		m.currentColumn = 0
+	if n.endIndex < m.NbColumns()-1 {
+		n.aroundSymbols = append(n.aroundSymbols, m.Content[n.lineIndex][n.endIndex+1])
+	}
+
+	start := max(0, n.startIndex-1)
+	stop := min(m.NbColumns()-1, n.endIndex+1)
+	for i := start; i <= stop; i++ {
+		if n.lineIndex > 0 {
+			n.aroundSymbols = append(n.aroundSymbols, m.Content[n.lineIndex-1][i])
+		}
+		if n.lineIndex < m.NbLines()-1 {
+			n.aroundSymbols = append(n.aroundSymbols, m.Content[n.lineIndex+1][i])
+		}
+	}
+}
+
+func (m *matrix) NextNumber() (engineNumber, error) {
+	if m.currentLine >= m.NbLines() {
+		return engineNumber{}, errors.New("End of matrix")
+	}
+	if m.currentColumn >= m.NbColumns() {
+		m.NextLine()
 		return m.NextNumber()
 	}
 
 	item := m.Content[m.currentLine][m.currentColumn]
+	regexp := regexp.MustCompile(`\d`)
+	number := regexp.FindString(item)
+	if number == "" {
+		m.MoveRight()
+		return m.NextNumber()
+	}
 	
-	m.currentColumn++
-	return number, nil
+	lastNbIndex := m.currentColumn
+	detectedNumber := []string{number}
+	for i := m.currentColumn+1; i < m.NbColumns(); i++ {
+		nextItem := m.Content[m.currentLine][i]
+		nextNumber := regexp.FindString(nextItem)
+		if nextNumber == "" {
+			break
+		}
+		lastNbIndex = i
+		detectedNumber = append(detectedNumber, nextNumber)
+	}
 
+	
+	n := engineNumber{
+		Value: strings.Join(detectedNumber, ""),
+		startIndex: m.currentColumn,
+		endIndex: lastNbIndex,
+		lineIndex: m.currentLine,
+	}
+
+	m.currentColumn = lastNbIndex + 1
+
+	m.FillAroundSymbols(&n)
+	return n, nil
+}
 
 func Day3() {
 	scanner, err := utils.ReadFileLines("resources/day3_input")
@@ -50,4 +136,19 @@ func Day3() {
 		lineChars := strings.Split(lineContent, "")
 		matrix.Content = append(matrix.Content, lineChars)
 	}
+
+	var res int
+	for {
+		n, err := matrix.NextNumber()
+		if err != nil {
+			break
+		}
+		fmt.Printf("Number %s : %s => engine part ? %t\n", n.Value, n.aroundSymbols, n.IsEnginePart())
+		if n.IsEnginePart() {
+			val, _ := strconv.Atoi(n.Value)
+			res += val
+		}
+	}
+
+	fmt.Printf("Result: %d\n", res)
 }
