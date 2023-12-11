b7ed5f782219c4bd7415caa9a97a5038bba1a7ac
Thomas Marquis <thomas.marquis.dev@gmail.com>
2023-12-11 21:58:14 +0100

    day 4 part 2 done

--- days/day4.go
+++ days/day4.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"sync"
 
 	"github.com/thomas-marquis/advent-of-code-2023/utils"
 )
@@ -11,6 +12,10 @@ import (
 
 var (
 	reg = regexp.MustCompile(`^Card\s*\d*:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s\|\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)$`)
+)
+
+const (
+	maxCardNb = 193
 )
 
 func ToIntSlice(strSlice []string) []int {
@@ -25,36 +30,88 @@ func ToIntSlice(strSlice []string) []int {
 	return intSlice
 }
 
+type Card struct {
+	Id int
+	NbMatches int
+	CopiedFromCard *Card
+	IsCopy bool
+}
+
 func Day4() {
 	scanner, _ := utils.ReadFileLines("resources/day4_input")
 
-	var res int
+	var cards []Card
+	
+	i := 1
 	for scanner.Scan() {
+		c := Card{
+			Id: i,
+			IsCopy: false,
+		}
+		
 		lineContent := scanner.Text()
 		groups := reg.FindStringSubmatch(lineContent)
-
+		
 		winningNbs := ToIntSlice(groups[1:11])
 		userNbs := ToIntSlice(groups[11:])
-
-		score := 0
-		var matchingNb []int
+		
 		for _, winN := range winningNbs {
 			for _, userN := range userNbs {
 				if winN == userN {
-					if score == 0 {
-						score = 1
-					} else {
-						score *= 2
-					}
-
-					matchingNb = append(matchingNb, winN)
+					c.NbMatches++
 				}
 			}
 		}
-		res += score
-
-		fmt.Printf("Score = %d ; Winning numbers: %#v\n", score, matchingNb)
+		
+		// fmt.Printf("Card %v: Winning numbers: %#v\n", c.Id, c.NbMatches)
+		
+		i++
+		cards = append(cards, c)
+	}
+	
+	var wg sync.WaitGroup
+	
+	res := len(cards)
+	cardsChan := make(chan Card)
+	for _, card := range cards {
+		wg.Add(1)
+		go getCardsCopies(card, cards, cardsChan, &wg)
 	}
 
+	for card := range cardsChan {
+		res++
+		if card.IsCopy {
+			fmt.Printf("Card %d copied from card %d (%d): Winning numbers: %#v => res=%d\n", card.Id, card.CopiedFromCard.Id, card.CopiedFromCard.NbMatches, card.NbMatches, res)
+		} else {
+			fmt.Printf("Original card %d: Winning numbers: %#v => res=%d\n", card.Id, card.NbMatches, res)
+		}
+		if card.NbMatches > 0 {
+			wg.Add(1)
+			go getCardsCopies(card, cards, cardsChan, &wg)
+		}
+	}
+
+	close(cardsChan)
+
+	wg.Wait()
+
+	fmt.Println(len(cards))
 	fmt.Println(res)
+}
+
+func getCardsCopies(card Card, cards []Card, ch chan Card, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	nbMatches := card.NbMatches
+	if nbMatches == 0 {
+		return
+	}
+
+	currCardId := card.Id
+	for i := currCardId + 1; i <= min(maxCardNb, currCardId + nbMatches); i++ {
+		copiedCard := cards[i-1]
+		copiedCard.CopiedFromCard = &card
+		copiedCard.IsCopy = true
+		ch <- copiedCard
+	}
 }
